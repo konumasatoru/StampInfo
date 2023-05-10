302c69b48c6997c5ac24354d16c7ac69a4b18253
EC2 Default User <ec2-user@ip-172-31-29-186.ap-northeast-1.compute.internal>
2023-05-10 16:47:42 +0000

    ちょっと進んだ

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,39 @@
 Rails.application.routes.draw do
-  # 顧客用
+
+# 管理者用
+# URL /admin/sign_in ...
+devise_for :admin, skip: [:registrations, :passwords], controllers: {
+  sessions: "admin/sessions",
+}
+
+namespace :admin do
+    get 'top' => 'homes#top', as: 'top'
+    get 'search' => 'homes#search', as: 'search'
+    resources :customers, only: [:index, :show, :edit, :update]
+    resources :posts, except: [:destroy]
+    resources :genres, only: [:index, :create, :edit, :update]
+  end
+  
+    # 顧客用
 # URL /customers/sign_in ...
 devise_for :customers,skip: [:passwords], controllers: {
   registrations: "public/registrations",
   sessions: 'public/sessions'
 }
 
-# 管理者用
-# URL /admin/sign_in ...
-devise_for :admin, skip: [:registrations, :passwords], controllers: {
-  sessions: "admin/sessions"
-}
-  get 'homes/top'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root to: "homes#top"
+scope module: :public do
+    root 'homes#top'
+    
+    get 'customers/mypage' => 'customers#show', as: 'mypage'
+    # customers/editのようにするとdeviseのルーティングとかぶってしまうためinformationを付け加えている。
+    get 'customers/information/edit' => 'customers#edit', as: 'edit_information'
+    patch 'customers/information' => 'customers#update', as: 'update_information'
+    get 'customers/unsubscribe' => 'customers#unsubscribe', as: 'confirm_unsubscribe'
+    put 'customers/information' => 'customers#update'
+    patch 'customers/withdraw' => 'customers#withdraw', as: 'withdraw_customer'
+    
+    resources :customers, only: [:index, :show, :edit, :update]
+    resources :posts, only: [:edit, :new, :index, :show, :create]
+    resources :posts, except: [:destroy]
+  end
 end
